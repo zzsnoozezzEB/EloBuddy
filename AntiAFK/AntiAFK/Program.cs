068ff02c9f20c948cf0792e3b2cbf0413649b779
zzsnoozezzEB <zz_snooze_zz@hotmail.com>
2016-04-11 09:42:19 +1000

    Initial

---
+++ AntiAFK/AntiAFK/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using SharpDX;
+using EloBuddy.SDK;
+
+namespace AntiAFK
+{
+    class Program
+    {
+        static int lastTick;
+        static int currentTick;
+        static Vector3 lastPos;
+        static Vector3 currentPos;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += gameLoadEventArgs =>
+            {
+                Hacks.AntiAFK = false;
+                lastTick = 0;
+                Game.OnUpdate += OnUpdate;
+            };
+        }
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (!ObjectManager.Player.IsDead)
+            {
+                currentTick = Environment.TickCount;
+                if (lastTick == 0)
+                    lastTick = Environment.TickCount;
+                currentPos = Player.Instance.ServerPosition;
+                if (lastPos.IsZero)
+                    lastPos = Player.Instance.ServerPosition;
+                if (currentPos.Distance(lastPos) > 200)
+                {
+                    lastTick = currentTick;
+                    lastPos = currentPos;
+                }
+                if ((currentTick - lastTick) > 70000)
+                    Game.QuitGame();
+            }
+        }
+    }    
+}
+
---
+++ CameraFocus/CameraFocus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using SharpDX;
+
+namespace CameraFocus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += GameLoaded;
+        }
+
+        private static void GameLoaded(EventArgs args)
+        {
+            Game.OnTick += OnGameTick;
+ 
+        }
+
+        private static void OnGameTick(EventArgs args) {
+            Vector2 diff;
+            var diffX = Player.Instance.Position.To2D().X - Camera.CameraX;
+            for (var i = 0; i < (int)diffX*100; i++)
+            {                
+                   Camera.CameraX = Camera.CameraX + diffX/100;
+            }
+
+
+        }
+
+    }
+}
